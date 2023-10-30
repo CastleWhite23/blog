4d82834820c9797e55d693216fdc77b7d77df2d8
= <=>
2023-10-30 12:11:13 -0300

    Estilizando o edit

--- views/layouts/edit.php
+++ views/layouts/edit.php
@@ -7,27 +7,29 @@ $post = edit();
 
 include(HEADER_TEMPLATE);
 ?>
-<style>
-    form {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        gap: 12px;
-    }
- </style>
-        <form action="edit.php?id=<?php echo $post['id_post']; ?>" method="POST">
 
-            <label for="titulo">Titulo:</label>
-            <input type="text" id="titulo" name="post['titulo_post']" value="<?php echo $post['titulo_post']; ?>" required>
-
-            <textarea type="text" cols="100" rows="40" name="post['conteudo_post']"
-                required><?php echo $post['conteudo_post']; ?></textarea>
-
-            <input type="submit" value="salvar"></input>
-            <a href="<?php echo BASEURL ?>/views/layouts/dashboard.php">Cancelar</a>
-        </form>
-
+        <div class="container mt-3 d-flex flex-column align-items-center">
+            <div class="row">
+                <h1>
+                    Crie suas postagens
+                </h1>
+            </div>
+            <div class="row d-flex flex-column align-items-center">
+                <form action="edit.php?id=<?php echo $post['id_post']; ?>" method="POST" class="form-post">
+                    <div class="mb-3 w-25 ">
+                        <input id="titulo" name="post['titulo_post']" value="<?php echo $post['titulo_post']; ?>" required class="form-control inpt-titulo" placeholder="Titulo:">
+                    </div>
+                    <div class="mb-3">
+                        <textarea type="text" cols="100" rows="35" name="post['conteudo_post']"  required class="form-input inpt-texto" id="exampleCheck1">
+                            <?php echo $post['conteudo_post']; ?>
+                        </textarea>
+                    </div>
+                    <button type="submit" class="btn btn-next">Salvar</button>
+                    <a href="<?php echo BASEURL ?>/views/layouts/dashboard.php">Cancelar</a>
+                </form>
+                
+            </div>
+        </div>
 <?php
 include(FOOTER_TEMPLATE);
 ?>
